f7f77a20edd279b7732557940119f378bc0b05d6
Camila Nori Dias Kubota <91036404+KubotaCND@users.noreply.github.com>
2024-02-05 19:50:18 -0300

    Add files via upload

---
+++ VisualStudioSolution/Credit Card/Cabeçalho.h
@@ -0,0 +1,14 @@
+#pragma once
+/*
+    Algorithm to validate a Credit Card Number;
+    Camila Nori Dias Kubota
+*/
+
+
+#include <stdio.h>
+#include <iostream>
+#include<string>
+#include<algorithm>
+#include<ctime>
+#include<sstream>
+#include<iomanip>
